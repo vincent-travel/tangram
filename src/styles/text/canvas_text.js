f2a3265198ebb4c4dbe4bb9dfe2eb210572685e8
David Valdman <dmvaldman@gmail.com>
2016-12-20 11:48:54 -0500

    RTL support

--- src/styles/text/canvas_text.js
+++ src/styles/text/canvas_text.js
@@ -54,7 +54,9 @@ export default class CanvasText {
 
                     if (text_settings.can_articulate){
                         let segments = splitLabelText(text);
+                        let rtl = isTextRTL(text);
 
+                        text_info.isRTL = rtl;
                         text_info.segments = segments;
                         text_info.size = [];
 
@@ -523,13 +525,22 @@ CanvasText.texcoord_cache = {};
 
 // Right-to-left / bi-directional text handling
 // Taken from http://stackoverflow.com/questions/12006095/javascript-how-to-check-if-character-is-rtl
-// function isRTL(s){
-//     var weakChars       = '\u0000-\u0040\u005B-\u0060\u007B-\u00BF\u00D7\u00F7\u02B9-\u02FF\u2000-\u2BFF\u2010-\u2029\u202C\u202F-\u2BFF',
-//         rtlChars        = '\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC',
-//         rtlDirCheck     = new RegExp('^['+weakChars+']*['+rtlChars+']');
+function isCharRTL(s){
+    var weakChars       = '\u0000-\u0040\u005B-\u0060\u007B-\u00BF\u00D7\u00F7\u02B9-\u02FF\u2000-\u2BFF\u2010-\u2029\u202C\u202F-\u2BFF',
+        rtlChars        = '\u0591-\u07FF\u200F\u202B\u202E\uFB1D-\uFDFD\uFE70-\uFEFC',
+        rtlDirCheck     = new RegExp('^['+weakChars+']*['+rtlChars+']');
 
-//     return rtlDirCheck.test(s);
-// }
+    return rtlDirCheck.test(s);
+}
+
+function isTextRTL(text){
+    for (let i = 0; i < text.length; i++){
+        if (isCharRTL(text[i])) {
+            return true;
+        }
+    }
+    return false;
+}
 
 // function reorderWordsLTR(words) {
 //     let words_LTR = [];
@@ -564,6 +575,7 @@ function splitLabelText(text){
 
     while (text.length){
         let segment = text.substring(0, codon_length);
+
         if (segment.length === 1) {
             segments[segments.length - 1] += segment;
         }
